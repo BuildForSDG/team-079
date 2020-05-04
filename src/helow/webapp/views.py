f58dc7eba6c9b4cec9b4920ac5ebfe9794e54174
Franklyn Ogbonna <franklynogbonna@Franklyns-MacBook-Pro.local>
2020-05-04 18:24:22 +0100

    fix: Adapt docstrings to pep257 covnvention to fix codacy warnings
    fix: Modify docstrings to fix codacy docstring warning
    
    fix: Adapt docstrings to pep257 docstring format
    
    fix: Adapt docstrings to  pep257 docstring format to fix warnings by codacy
    
    fix: Add pep257 dev package to validate pep257 docstring format

--- src/helow/webapp/views.py
+++ src/helow/webapp/views.py
@@ -1,12 +1,15 @@
+"""Manage views functions for webapp."""
 from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
-    msg = """
+    """View function for home route."""
+    msg = """HELOW API DOCS
+    
     Welcome to HELOW API Backend! HElp is On the Way! Kindly access the API documentation using any of the following
-    for more information:\n
-    http://127.0.0.1:8000/api/v1/swagger/,\n
+    for more information:
+    http://127.0.0.1:8000/api/v1/swagger/,
     http://127.0.0.1:8000/api/v1/redoc/,
     http://127.0.0.1:8000/docs/
     """
