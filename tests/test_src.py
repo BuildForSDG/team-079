d90c9939c9a4975e6f055c72eccddf7b5539c1f8
Franklyn Ogbonna <franklynogbonna@Franklyns-MacBook-Pro.local>
2020-05-04 16:26:37 +0100

    fix: Remove the parenthesis after the view name object to fix error

--- tests/test_src.py
+++ tests/test_src.py
@@ -12,4 +12,8 @@ def test_version():
 class UrlsTest(TestCase):
     def test_root_resolves_to_home(self):
         main_page = resolve('/')
-        self.assertEqual(main_page.func, views.home())
+        self.assertEqual(main_page.func, views.home)
+
+    def test_home_returns_successful(self):
+        home = self.client.get('/')
+        self.assertEquals(home.status_code, 200)
