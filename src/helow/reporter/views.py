f4176fc97d3c105cda1300a8b86e2b56065b6ae5
haffs <abuafeezabolarinwa@yahoo.com>
2020-05-18 11:03:04 +0100

    update

--- src/helow/reporter/views.py
+++ src/helow/reporter/views.py
@@ -37,8 +37,7 @@ class IncidentListView(generics.ListAPIView):
         elif incident_type:
             queryset = queryset.filter(incident_type__label=incident_type)
         return queryset.order_by('-reported_at')
-    
-    
+      
 class IncidentTypesViewset(viewsets.ModelViewSet):
     queryset = IncidentType.objects.all()
     serializer_class = serializers.IncidentTypeSerializer
