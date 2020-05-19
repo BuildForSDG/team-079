f823dcb66fc82ab925b6a0d1b6d678b874aa2c37
Franklyn Ogbonna <franklynogbonna@Franklyns-MacBook-Pro.local>
2020-05-19 12:50:38 +0100

    feat: use google map api to get nearby responders to an incident

--- src/helow/responder/urls.py
+++ src/helow/responder/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ResponderViewset, ResponderFilteredView
+from .views import ResponderViewset, LocationView, find_responder
 
 router = DefaultRouter()
 router.register('responder', ResponderViewset, basename='responder')
 
 urlpatterns = [
-    path('responder/find/', ResponderFilteredView.as_view(), name='find_responder'),
+    path('responder/find/', find_responder, name='find_responder'),
+    path('incident/location/', LocationView.as_view(), name='incident_location'),
 ]
 
 urlpatterns += router.urls
--- src/helow/responder/views.py
+++ src/helow/responder/views.py
@@ -1,5 +1,6 @@
 """Views for responder."""
 from rest_framework import viewsets, generics
+
 from .models import Responder
 from .serializers import ResponderSerializer
 
@@ -9,20 +10,49 @@ class ResponderViewset(viewsets.ModelViewSet):
     serializer_class = ResponderSerializer
 
 
-class ResponderFilteredView(generics.ListAPIView):
-    serializer_class = ResponderSerializer
-
-    def get_queryset(self):
-        queryset = Responder.objects.all()
-        incident_id = self.request.query_params.get('incident', None)
-
-        from reporter.models import IncidentReport
-        from django.shortcuts import get_object_or_404
-
-        incident = get_object_or_404(IncidentReport, pk=incident_id)
-        latitude = incident.location.latitude
-        longitude = incident.location.longitude
-        print(longitude, latitude)
+class LocationView(generics.ListAPIView):
+    from reporter.models import IncidentLocation
+    from reporter.serializers import IncidentLocationSerializer
+    queryset = IncidentLocation.objects.all()
+    serializer_class = IncidentLocationSerializer
 
 
-        return queryset.order_by('id')
+def process_incident_type(incident_type):
+    place_type = ''
+    if incident_type == 'Arm Robbery':
+        place_type = 'police'
+    elif incident_type in ['Pedestrian Knock Down', 'Hit and Run', 'Motor Vehicle Collision', 'Uncategorized']:
+        place_type = 'hospital'
+    return place_type
+
+
+def find_responder(request):
+    from reporter.models import IncidentReport
+    from django.shortcuts import get_object_or_404
+    from django.http import JsonResponse
+    import requests
+    import json
+
+    incident_id = request.GET.get('incident')
+
+    incident = get_object_or_404(IncidentReport, pk=incident_id)
+    latitude = incident.location.latitude
+    longitude = incident.location.longitude
+    location = f'{str(latitude)},{str(longitude)}'
+    place_type = process_incident_type(incident.incident_type.label)
+
+    # construct the query parameters
+    payload = {
+        'location': location,
+        'radius': 500,
+        'types': place_type,
+        'key': 'AIzaSyBr427Ow25KCsgHcHQe_J1V1L39nTouIfk'
+    }
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+    response = requests.get(url, params=payload)
+
+    # get the first five places
+    MAX_OBJECT = 5
+    rsp = json.loads(response.text)['results'][:MAX_OBJECT]
+
+    return JsonResponse(rsp, safe=False)
